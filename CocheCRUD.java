3a5a5a72e0db86eaf6dd64049aa5ab231de421bf
diegofeva <96393362+diegofeva@users.noreply.github.com>
2021-12-19 22:21:02 +0100

    Add files via upload

---
+++ CocheCRUD.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.List;
+
+public interface CocheCRUD {
+
+    void save(Coche coche);
+    List<Coche> findAll();
+    void delete(Coche coche);
+}
--- main.java
+++ main.java
@@ -2,36 +2,18 @@ package com.example;
 
 public class main {
 
+    static CocheCRUD cocheCRUD = new CocheCRUDImpl();
+
     public static void main(String[] args) {
 
-        SmartDevice smartDevice = new SmartDevice();
-        SmartPhone smartPhone = new SmartPhone();
-        SmartWatch smartWatch = new SmartWatch();
 
-        SmartDevice galaxyTablet = new SmartDevice("Galaxy Tablet", 10.5,1500,true,"Usb C", "Android",4.5);
-        SmartPhone galaxyPhone = new SmartPhone("GalaxyPhone",5,4,58);
-        SmartWatch galaxyWatch = new SmartWatch("GalaxyWatch","Nilon",true);
+        cocheCRUD.save(new Coche());
+        cocheCRUD.delete(new Coche());
+        cocheCRUD.findAll();
 
 
-        System.out.println(galaxyTablet.modelo);
-        System.out.println(galaxyTablet.screenSize);
-        System.out.println(galaxyTablet.capacidadBateria);
-        System.out.println(galaxyTablet.tactil);
-        System.out.println(galaxyTablet.tipoDeCargador);
-        System.out.println(galaxyTablet.sistemaOperativo);
-        System.out.println(galaxyTablet.version);
-        System.out.println("");
 
 
-        System.out.println(galaxyPhone.modelo);
-        System.out.println(galaxyPhone.numeroBotones);
-        System.out.println(galaxyPhone.cantidadCamaras);
-        System.out.println(galaxyPhone.megaPixeles);
-        System.out.println("");
 
-
-        System.out.println(galaxyWatch.modelo);
-        System.out.println(galaxyWatch.materialCorrea);
-        System.out.println(galaxyWatch.pantallaColor);
     }
 }
